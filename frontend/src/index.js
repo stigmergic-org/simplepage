5ad64bd21c0f835bd490b8ea2136f8b67e23dada
Joel Thorstensson <joel@stigmergic.org>
2025-07-16 16:33:55 +0100

    feat(frontend): add plausible analytics

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './app';
+import Plausible from 'plausible-tracker'
+
+// Initialize Plausible analytics
+const { enableAutoPageviews } = Plausible({
+  domain: 'simplepage.eth.link',
+  trackLocalhost: true,
+})
+enableAutoPageviews()
+
 
 const rootElement = document.getElementById('root');
 const existingContent = document.getElementById('content-container')?.innerHTML;
